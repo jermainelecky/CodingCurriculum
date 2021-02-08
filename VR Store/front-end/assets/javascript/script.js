3142e0bf9097823aa3b19aac39db81996ea3efa2
Jermaine <45435946+jermainelecky@users.noreply.github.com>
2021-02-08 10:00:45 -0500

    Delete VR Store directory

--- VR Store/front-end/assets/javascript/script.js
+++
@@ -1,8 +0,0 @@
-function openMobileNav() {
-    let mobileNav = document.getElementById("mobile-links");
-    if (mobileNav.style.display === "none") {
-        mobileNav.style.display = "flex";
-    } else {
-        mobileNav.style.display = "none";
-    }
-}
